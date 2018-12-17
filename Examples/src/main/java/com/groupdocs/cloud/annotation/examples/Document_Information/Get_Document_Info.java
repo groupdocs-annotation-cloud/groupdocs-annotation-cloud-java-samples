0585ef02f28a8ab5dae4895d6bde9ac9029d1e35
Muhammad Rizwan Niazi <muhammad.rizwan@groupdocs.com>
2018-12-18 01:10:36 +0500

    GroupDocs.Annotation Cloud Java SDK Sample Code

--- Examples/src/main/java/com/groupdocs/cloud/annotation/examples/Document_Information/Get_Document_Info.java
+++ Examples/src/main/java/com/groupdocs/cloud/annotation/examples/Document_Information/Get_Document_Info.java
@@ -3,9 +3,8 @@ package com.groupdocs.cloud.annotation.examples.Document_Information;
 import com.groupdocs.cloud.annotation.model.*;
 import com.groupdocs.cloud.annotation.model.requests.*;
 import com.groupdocs.cloud.annotation.examples.Utils;
-import com.groupdocs.cloud.annotation.api.AnnotationApi;
+import com.groupdocs.cloud.annotation.api.ImageInfoApi;
 import com.groupdocs.cloud.annotation.client.ApiException;
-import java.util.List;
 
 public class Get_Document_Info {
 
@@ -13,17 +12,17 @@ public class Get_Document_Info {
 
 		try {
 
-			// Create and get annotation api instance
-			AnnotationApi apiInstance = Utils.getAnnotationAPIInstance();
+			// Create and get ImageInfo api instance
+			ImageInfoApi apiInstance = Utils.getImageInfoApiInstance();
 
 			// create request instance
-			GetImportRequest request = new GetImportRequest();
+			GetInfoRequest request = new GetInfoRequest();
 			request.setName("one-page-annotated.pdf");
 			request.setFolder("annotationdocs");
 			request.setPassword("");
 
 			// Call api with request
-			DocumentInfo response = apiInstance.getImport(request);
+			DocumentInfo response = apiInstance.getInfo(request);
 
 			// Get and write output
 			System.out.println("DocumentInfo: " + response.getName());
